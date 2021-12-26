08e248fb0cab2266af44401233c51216df36f208
Domirando <maisiedev@gmail.com>
2021-12-26 11:36:23 +0500

    changed

--- action/hackathon.js
+++ action/hackathon.js
@@ -26,5 +26,5 @@ bot.command('hackathon', ctx => {
 })
 
 bot.action("presentation", ctx => {
-    ctx.replyWithDocument({source: "assets/winter_web_hackathon.pdf", filename: "Winter Web Hackathon.pdf", caption: "something"}).then()
+    ctx.replyWithDocument({source: "assets/winter_web_hackathon.pdf", filename: "Winter Web Hackathon.pdf"}, { caption: "Bot: @winter_hackathon_bot"})
 })

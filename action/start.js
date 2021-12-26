81be1148daa54c6400d8ec20de8bd887e9ca81ce
Domirando <maisiedev@gmail.com>
2021-12-26 07:56:35 +0500

    Changed

--- action/start.js
+++ action/start.js
@@ -6,9 +6,9 @@ const {bot} = require('../core/bot')
 const composer = new Composer()
 
 composer.start(ctx => {
-  ctx.replyWithSticker(messages["greeting_gif"])
+  ctx.replyWithSticker(messages.greeting_gif)
     .catch(e => console.error(e.message))
-  ctx.replyWithHTML(messages["start"])
+  ctx.replyWithHTML(messages.start(ctx))
     .catch(e => console.error(e.message))
 })
 

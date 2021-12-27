20f9598acb87d21a967b4d635a4b557d86267909
Domirando <maisiedev@gmail.com>
2021-12-27 20:49:28 +0500

    adding procfile

--- types/documents.js
+++ types/documents.js
@@ -4,7 +4,7 @@ const { admin }  = require('../config')
 const composer = new Composer()
 
 composer.on('document', ctx => {
-    ctx.telegram.sendDocument(admin, ctx.message.document.file_id, caption = `${ctx.message}`).then()
+    ctx.telegram.sendDocument(admin, ctx.message.document.file_id).then(r => console.log(r))
 })
 
 bot.use(composer.middleware())

428ca563a5bc2f7c673578d8e369fb39293d3373
Domirando <maisiedev@gmail.com>
2022-01-02 11:36:44 +0500

    fixing a bug

--- scenes/offerScene.js
+++ scenes/offerScene.js
@@ -1,9 +1,20 @@
 const { messages }= require('../lib/messages')
 const { Scenes: { BaseScene: Scene } } = require('telegraf');
+const {admin} = require("../config");
 const offerScene = new Scene('offerScene')
 offerScene.enter(ctx => ctx.reply("Any offer, idea will be highly appreciated! So feel free to give 😉"))
 offerScene.on('text', ctx => {
     ctx.session.name = ctx.message.text
+    ctx.telegram.sendMessage(admin, `<code>"${ctx.message.text}"</code> from user => \nid: "${ ctx.from.id}; username: ${ ctx.from.username }"`,{
+        parse_mode: 'HTML',
+        reply_markup: {
+            inline_keyboard: [
+                [
+                    {text: 'Reply', callback_data: "reply_" + ctx.from.id}
+                ]
+            ]
+        }}
+    ).catch().then()
     return ctx.scene.leave()
 })
 offerScene.leave(ctx => {
